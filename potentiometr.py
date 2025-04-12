cc2f63657150c35b9f37aa4e1ba044a430680e91
Arman <iperov.55@gmail.com>
2025-04-12 18:41:43 +0400

    final commit

--- potentiometr.py
+++ potentiometr.py
@@ -1,29 +1,56 @@
 import RPi.GPIO as GPIO
-import time
+import spidev
+from time import sleep
 
-potInp = 18
-ledPin = 4
+ledPin = 17
+
+spi = None
+pwm = None
+
 
 def setup():
+    
+    global spi, pwm
+
+
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(ledPin, GPIO.OUT)
-    GPIO.setup(potInp, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    spi = spidev.SpiDev()
+    spi.open(0, 0)
+    spi.max_speed_hz = 50000
+    spi.mode = 0b00
+    pwm = GPIO.PWM(ledPin, 1000)
+    pwm.start(0)
 
 
-def loop():
-    while True:
-            print(GPIO.input(potInp), 'potentiometrinp')
-           
-            
+def read_channel(channel):
+    
+    if channel < 0 or channel > 7:
+        return -1
+    
+    adc = spi.xfer2([1, (8 + channel) << 4, 0])
+    data = ((adc[1] & 3) << 8) | adc[2]
+    return data
+
 
 def destroy():
     GPIO.output(ledPin, GPIO.LOW)     
-    GPIO.cleanup() 
+    GPIO.cleanup()
+    spi.close()
 
 if __name__ == '__main__':     
     print ('Program is starting...')
     setup()
     try:
-        loop()
+        while True:
+            value = read_channel(0)
+            if value < 5:
+                brightness = 0
+            else:    
+                brightness = (value / 1023.0) * 100
+            pwm.ChangeDutyCycle(brightness)
+            print(f"Potentiometer value: {value} -> LED brightness: {brightness}%")
+            sleep(0.4)
+
     except KeyboardInterrupt:  
-        destroy()
+        destroy()    
